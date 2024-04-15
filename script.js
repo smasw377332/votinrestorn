8bf95c45118871521d85055a53b673be33e0a668
smasw377332 <156338115+smasw377332@users.noreply.github.com>
2024-04-15 19:53:24 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function p () {
+alert(' telegram: @EgorikSofr')
+}
+function p () {
+   alert("telegram: @EgorikSofr")
+}
+
+
+let str = "Чтобы выбрать нажми на логотип"
+str  =str.replace('','')
+alert (str)
+
+ calc ()
+ var a = document.getElementById('name')
+ a.style.backgroundColor = "red"
+ function test_us () {
+    var arr = ['ref','fw4','three','frfre'] ;
+    alert(arr [0]);
+ }
+ test_us()
